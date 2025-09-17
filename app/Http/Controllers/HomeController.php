ca7b021dcebbd8f1ff18e55d5dfbc4670e38ec5a
batistalando7 <batistalando7@gmail.com>
2025-09-17 14:29:49 +0100

    Subindo o modal pesquizador de univesidade

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -21,8 +21,8 @@ class HomeController extends Controller
      *
      * @return \Illuminate\Contracts\Support\Renderable
      */
-    public function index()
+    /* public function index()
     {
         return view('_admin.dashboard.crm.index');
-    }
+    } */
 }
