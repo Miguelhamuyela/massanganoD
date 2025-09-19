43ef895ab035d00498cece92378971d855553d06
unknown <Administrador@Desenvolvedor10.infosi.local>
2025-09-19 12:58:43 +0100

    modal subscribe

---
+++ app/Mail/contatoMail.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class contatoMail extends Mailable
+{
+    use Queueable, SerializesModels;
+    
+    public $mensagem;
+
+    public function __construct($mensagem)
+    {
+        $this->mensagem = $mensagem;
+    }
+
+    public function build()
+    {
+        return $this->subject('Novo Contato')
+            ->view('emails.contato', [
+                'mensagem' => $this->mensagem
+            ]);
+    }
+}
